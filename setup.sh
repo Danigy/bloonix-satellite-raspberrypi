c86dd8fdaefc73318f9fbdbfcc6fa31e12511811
Peter Thurner <p.thurner@blunix.org>
2016-12-15 06:44:32 +0100

    fixed /etc/hosts setup

--- setup.sh
+++ setup.sh
@@ -67,7 +67,8 @@ ORIGIN="$(whois $PUBLIC_IP | grep origin | awk '{print $2}')"
 full_host_name="${ORIGIN,,}.${DOMAIN}"
 hostname $full_host_name
 sed '/127.0.1.1/d' /etc/hosts
-echo "127.0.1.1 $full_host_name ${ORIGIN,,}" >> /etc/hosts
+echo "127.0.0.1 localhost
+127.0.1.1 $full_host_name ${ORIGIN,,}" >> /etc/hosts
 echo $full_host_name > /etc/hostname
 
 # Setup a nice PS1 so the hostname is viewd
